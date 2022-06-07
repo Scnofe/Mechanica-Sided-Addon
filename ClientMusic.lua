85ce928b88dc3d10ab7ba1963253d57251f0963b
N275 <69851675+LikedLeaf@users.noreply.github.com>
2022-06-07 15:36:54 +0800

    Update ClientMusic.lua

--- ClientMusic.lua
+++ ClientMusic.lua
@@ -1,4 +1,4 @@
-wait(3)
+wait(1)
 local Player = game:GetService("Players")
 local PlayerGui = Player.LocalPlayer.PlayerGui:FindFirstChild("MainGui")
 
@@ -93,136 +93,157 @@ if FrameTwo_:FindFirstChild("A") and FrameTwo_:FindFirstChild("B") then
 	end
 	wait(1)
 	FrameOne_:Destroy()
-
-	local Sizer = Instance.new("Frame")
-	Sizer.SizeConstraint = Enum.SizeConstraint.RelativeXX
-	Sizer.Parent = PlayerGui
-	Sizer.Name = "AudioPlayer"
-	Sizer.BackgroundTransparency = 1
-	Sizer.ClipsDescendants = true
-	Sizer:TweenSize(UDim2.new(0.152, 0,0.121, 0))
-	Sizer:TweenPosition(UDim2.new(0.848, 0,0.174, 0))
-
-	local FrameThree_ = Instance.new("Frame")
-	FrameThree_.Parent = Sizer
-	FrameThree_.BackgroundColor3 = Color3.fromRGB(48, 48, 48)
-	FrameThree_.BorderSizePixel = 0
-	FrameThree_:TweenSize(UDim2.new(0.997, 0,0.802, 0))
-	FrameThree_:TweenPosition(UDim2.new(0.003, 0,0.198, 0))
-
-	local UiButton = Instance.new("TextButton")
-	UiButton.BackgroundColor3 = Color3.fromRGB(48, 48, 48)
-	UiButton.Name = "Ui"
-	UiButton.BorderSizePixel = 0
-	UiButton.Parent = FrameThree_
-	UiButton:TweenSize(UDim2.new(0.417, 0,0.079, 0))
-	UiButton:TweenPosition(UDim2.new(0, 0,-0.079, 0))
-	UiButton.TextColor3 = Color3.fromRGB(255,255,255)
-	UiButton.Text = "Close"
-	UiButton.TextScaled = true
-	UiButton.TextStrokeTransparency = 0.85
-
 end
 
-local AudioPlayer = PlayerGui:FindFirstChild("AudioPlayer")
+local BackgroundSize = Instance.new("Frame")
+BackgroundSize.Name = "BackgroundSize"
+BackgroundSize.BackgroundTransparency = 1
+BackgroundSize.Parent = PlayerGui
+BackgroundSize:TweenSize(UDim2.new(1,0,1,0))
+BackgroundSize.ZIndex = 4
 
-local ToggleUi = true
-AudioPlayer.Frame.Ui.MouseButton1Down:Connect(function()
-	if ToggleUi == true then
-		AudioPlayer.Frame.Ui.Text = "Open"
-		AudioPlayer.Frame:TweenPosition(UDim2.new(0, 0, 1, 0))
-		ToggleUi = false
-	elseif ToggleUi == false then
-		AudioPlayer.Frame.Ui.Text = "Close"
-		AudioPlayer.Frame:TweenPosition(UDim2.new(0, 0, 0, 0))
-		ToggleUi = true
+local FrameInside = Instance.new("Frame")
+FrameInside.Parent = BackgroundSize
+FrameInside.BackgroundColor3 = Color3.fromRGB(52, 52, 52)
+FrameInside:TweenSize(UDim2.new(0.194, 0,1, 0))
+FrameInside:TweenPosition(UDim2.new(0.806, 0,0, 0))
+
+local UiButtonOne_ = Instance.new("TextButton")
+UiButtonOne_.Name = "UiButton"
+UiButtonOne_.Parent = FrameInside
+UiButtonOne_.Text = "Close"
+UiButtonOne_:TweenSize(UDim2.new(0.293, 0,0.031, 0))
+UiButtonOne_:TweenPosition(UDim2.new(-0.295, 0,0.969, 0))
+UiButtonOne_.BackgroundTransparency = 1
+UiButtonOne_.TextColor3 = Color3.fromRGB(255,255,255)
+UiButtonOne_.TextStrokeTransparency = 0.85
+UiButtonOne_.TextScaled = true
+
+local TitleOne_ = Instance.new("TextLabel")
+TitleOne_.Name = "Title"
+TitleOne_.Parent = FrameInside
+TitleOne_.Text = "SoundId"
+TitleOne_.BackgroundTransparency = 1
+TitleOne_.TextColor3 = Color3.fromRGB(255,255,255)
+TitleOne_.TextStrokeTransparency = 0.85
+TitleOne_.TextScaled = true
+TitleOne_:TweenSize(UDim2.new(0.263, 0,0.033, 0))
+TitleOne_:TweenPosition(UDim2.new(0.038, 0,0.021, 0))
+
+local TextBoxSound = Instance.new("TextBox")
+TextBoxSound.Name = "SoundBox"
+TextBoxSound.ClearTextOnFocus = false
+TextBoxSound.Parent = FrameInside
+TextBoxSound.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+TextBoxSound.PlaceholderText = "Write in SoundId"
+TextBoxSound.Text = ""
+TextBoxSound.TextColor3 = Color3.fromRGB(255,255,255)
+TextBoxSound.TextStrokeTransparency = 0.85
+TextBoxSound.BorderSizePixel = 0
+TextBoxSound.TextScaled = true
+TextBoxSound:TweenSize(UDim2.new(0.938, 0,0.033, 0))
+TextBoxSound:TweenPosition(UDim2.new(0.036, 0,0.054, 0))
+
+local UiCorner = Instance.new("UICorner")
+UiCorner.Parent = TextBoxSound
+UiCorner.Name = "Corner"
+UiCorner.CornerRadius = UDim.new(0,0)
+local USX = 0
+while USX < 8 do
+	USX += 0.2
+	UiCorner.CornerRadius = UDim.new(0,USX)
+	wait()
+end
+
+local ImageButton = Instance.new("ImageButton")
+ImageButton.Name = "PlayButton"
+ImageButton.Parent = FrameInside
+ImageButton.BackgroundTransparency = 1
+ImageButton.Image = "http://www.roblox.com/asset/?id=3192518259"
+ImageButton.ScaleType = Enum.ScaleType.Fit
+ImageButton:TweenSize(UDim2.new(0.048, 0,0.023, 0))
+ImageButton:TweenPosition(UDim2.new(0.08, 0,0.094, 0))
+
+
+
+local LoopLabel = Instance.new("TextLabel")
+LoopLabel.Name = "Title2"
+LoopLabel.Parent = FrameInside
+LoopLabel.Text = "Loop(Optional)"
+LoopLabel.BackgroundTransparency = 1
+LoopLabel.TextColor3 = Color3.fromRGB(255,255,255)
+LoopLabel.TextStrokeTransparency = 0.85
+LoopLabel.TextScaled = true
+LoopLabel:TweenSize(UDim2.new(0.263, 0,0.028, 0))
+LoopLabel:TweenPosition(UDim2.new(0.71, 0,0.093, 0))
+
+local ButtonInstance = Instance.new("TextButton")
+ButtonInstance.Name = "Loops"
+ButtonInstance.Parent = FrameInside
+ButtonInstance.Text = "False"
+ButtonInstance.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+ButtonInstance.TextColor3 = Color3.fromRGB(255,255,255)
+ButtonInstance.TextStrokeTransparency = 0.85
+ButtonInstance.TextScaled = true
+ButtonInstance:TweenSize(UDim2.new(0.263, 0,0.028, 0))
+ButtonInstance:TweenPosition(UDim2.new(0.71, 0,0.123, 0))
+ButtonInstance.ZIndex = 5
+
+local UiCorner2 = Instance.new("UICorner")
+UiCorner2.Parent = ButtonInstance
+UiCorner2.Name = "Corner"
+UiCorner2.CornerRadius = UDim.new(0,0)
+local USX2 = 0
+while USX2 < 8 do
+	USX2 += 0.2
+	UiCorner2.CornerRadius = UDim.new(0,USX2)
+	wait()
+end
+
+local TheMainUi = PlayerGui.BackgroundSize
+
+local SoundInt = Instance.new("Sound")
+SoundInt.Parent = TheMainUi.Frame
+
+local Toggle = true
+TheMainUi.Frame.UiButton.MouseButton1Down:Connect(function()
+	if Toggle == true then
+		TheMainUi.Frame.UiButton.Text = "Open"
+		TheMainUi.Frame:TweenPosition(UDim2.new(1, 0,0, 0))
+		Toggle = false
+	elseif Toggle == false then
+		TheMainUi.Frame.UiButton.Text = "Close"
+		TheMainUi.Frame:TweenPosition(UDim2.new(0.806, 0,0, 0))
+		Toggle = true
 	end
 end)
 
-local AudioForPlay = Instance.new("Sound")
-AudioForPlay.Parent = AudioPlayer
-
-local Title1 = Instance.new("TextLabel")
-Title1.BorderSizePixel = 0
-Title1.Name = "Title1"
-Title1.BackgroundTransparency = 1
-Title1.TextColor3 = Color3.fromRGB(255,255,255)
-Title1.TextStrokeTransparency = 0.85
-Title1.TextScaled = true
-Title1.Text = "SoundId"
-Title1.Parent = AudioPlayer.Frame
-Title1:TweenPosition(UDim2.new(0.035, 0,0.082, 0))
-Title1:TweenSize(UDim2.new(0.273, 0,0.079, 0))
-
-local TextBoxInt = Instance.new("TextBox")
-TextBoxInt.Name = "AudioTextBox"
-TextBoxInt.Text = ""
-TextBoxInt.PlaceholderText = "Type in SoundId"
-TextBoxInt.TextColor3 = Color3.fromRGB(255,255,255)
-TextBoxInt.TextStrokeTransparency = 0.85
-TextBoxInt.TextScaled = true
-TextBoxInt.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
-TextBoxInt.BorderSizePixel = 0
-TextBoxInt.Parent = AudioPlayer.Frame
-TextBoxInt:TweenSize(UDim2.new(0.669, 0,0.079, 0))
-TextBoxInt:TweenPosition(UDim2.new(0.309, 0,0.082, 0))
-
-wait()
-local PlayB = Instance.new("TextButton")
-PlayB.Name = "PlayButton"
-PlayB.TextScaled = true
-PlayB.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
-PlayB.BorderSizePixel = 0
-PlayB.TextColor3 = Color3.fromRGB(255,255,255)
-PlayB.Parent = AudioPlayer.Frame
-PlayB.Text = "Play"
-PlayB:TweenSize(UDim2.new(0.143, 0,0.079, 0))
-PlayB:TweenPosition(UDim2.new(0.307, 0,0.197, 0))
-
-local PauseB = Instance.new("TextButton")
-PauseB.Name = "PauseButton"
-PauseB.TextScaled = true
-PauseB.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
-PauseB.BorderSizePixel = 0
-PauseB.TextColor3 = Color3.fromRGB(255,255,255)
-PauseB.Parent = AudioPlayer.Frame
-PauseB.Text = "Pause"
-PauseB:TweenSize(UDim2.new(0.143, 0,0.079, 0))
-PauseB:TweenPosition(UDim2.new(0.448, 0,0.197, 0))
-
-local LoopB = Instance.new("TextButton")
-LoopB.Name = "Loops"
-LoopB.TextScaled = true
-LoopB.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
-LoopB.BorderSizePixel = 0
-LoopB.TextColor3 = Color3.fromRGB(255,255,255)
-LoopB.Parent = AudioPlayer.Frame
-LoopB.Text = "Loop It"
-LoopB:TweenSize(UDim2.new(0.16, 0,0.079, 0))
-LoopB:TweenPosition(UDim2.new(0.59, 0,0.197, 0))
-
-AudioPlayer.Frame.AudioTextBox.FocusLost:Connect(function()
-	if not(tonumber(AudioPlayer.Frame.AudioTextBox.Text)) then
-		AudioPlayer.Frame.AudioTextBox.Text = ""
+TheMainUi.Frame.SoundBox.FocusLost:Connect(function()
+	if not(tonumber(TheMainUi.Frame.SoundBox.Text)) then
+		TheMainUi.Frame.SoundBox.Text = ""
 	else
-		AudioPlayer.Sound.SoundId = "rbxassetid://"..AudioPlayer.Frame.AudioTextBox.Text
+		TheMainUi.Frame.Sound.SoundId = "rbxassetid://"..TheMainUi.Frame.SoundBox.Text
 	end
 end)
 
-AudioPlayer.Frame.PlayButton.MouseButton1Down:Connect(function()
-	AudioPlayer.Sound.Playing = true
+TheMainUi.Frame.PlayButton.MouseButton1Down:Connect(function()
+	if TheMainUi.Frame.Sound.Playing == false then
+		TheMainUi.Frame.PlayButton.Image = "http://www.roblox.com/asset/?id=3192517628"
+		TheMainUi.Frame.Sound.Playing = true
+	elseif TheMainUi.Frame.Sound.Playing == true then
+		TheMainUi.Frame.PlayButton.Image = "http://www.roblox.com/asset/?id=3192518259"
+		TheMainUi.Frame.Sound.Playing = false
+	else
+		print("Audio Failed")
+	end
 end)
 
-AudioPlayer.Frame.PauseButton.MouseButton1Down:Connect(function()
-	AudioPlayer.Sound.Playing = false
-end)
-
-AudioPlayer.Frame.Loops.MouseButton1Down:Connect(function()
-	if AudioPlayer.Sound.Looped == true then
-		AudioPlayer.Frame.Loops.Text = "Loop It"
-		AudioPlayer.Sound.Looped = false
-	elseif AudioPlayer.Sound.Looped == false then
-		AudioPlayer.Frame.Loops.Text = "Looping"
-		AudioPlayer.Sound.Looped = true
+TheMainUi.Frame.Loops.MouseButton1Down:Connect(function()
+	if TheMainUi.Frame.Sound.Looped == true then
+		TheMainUi.Frame.Loops.Text = "false"
+		TheMainUi.Frame.Sound.Looped = false
+	elseif TheMainUi.Frame.Sound.Looped == false then
+		TheMainUi.Frame.Loops.Text = "true"
+		TheMainUi.Frame.Sound.Looped = true
 	end
 end)
